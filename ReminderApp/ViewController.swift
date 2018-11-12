13294b5c04eef6fd26761490a2deae6a2043ad39
Fahri MacBook <fahri-can@web.de>
2018-11-12 20:45:08 +0100

    Start new iOS Project.

--- ReminderApp/ViewController.swift
+++ ReminderApp/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import UserNotifications
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
+    
+    
+    @IBOutlet weak var userInput: UITextField!
+    
+    @IBAction func confirm(_ sender: Any) {
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]){ (granted, error) in }
+        
+        UNUserNotificationCenter.current().delegate = self
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Shopping List"
+        content.subtitle = "Reminder"
+        content.body = "Buy Milk"
+        content.badge = 1
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let requestId = "requestID"
+        let request = UNNotificationRequest(identifier: requestId, content: content, trigger: trigger)
+        
+        let userNotificationCenter = UNUserNotificationCenter.current().add(request, withCompletionHandler: {(error) in })
+        
     }
-
-
+    
+    
+    // This method will be called when app received push notifications in foreground
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void)
+    {
+        completionHandler([.alert, .badge, .sound])
+    }
+    
+    
 }
 
